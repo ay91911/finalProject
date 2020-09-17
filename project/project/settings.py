8e5c0c00edd23cb144b573e3388da2cd1a56b610
mechurisr <mechurisr@naver.com>
2020-09-17 10:56:38 +0900

    settings_app_added_20200917

--- project/project/settings.py
+++ project/project/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'empathy.apps.EmpathyConfig'
+    'smile.apps.SmileConfig'
+    'status.apps.StatusConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
