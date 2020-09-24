7ecc18351aeea87abb303caf6f1764310e2cfe27
roufspice <daeyoun.ha27@gmail.com>
2020-09-24 17:48:47 +0900

    2020-09-25-v2

--- project/smile/views.py
+++ project/smile/views.py
@@ -3,19 +3,50 @@ from django.views.decorators import gzip
 from django.http import StreamingHttpResponse, HttpResponseServerError
 import cv2
 from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.image import img_to_array
+import numpy as np
+from statistics import mode
 
 context = {
         'title': 'Home'
     }
 
+#model path
+detection_model_path = 'C:/dev/finalProject/project/smile/detection_models/haarcascade_frontalface_default.xml'
+emotion_model_path = 'C:/dev/finalProject\project\smile\emotion_models\_vgg16_01_.34-0.77-0.6478.h5'
+emotion_labels =["happy","angry","sad","neutral","surprise"]
 
+
+#initialization
+frame_window = 10
+emotion_window =[]
 
 class VideoCamera:
+    global detection_model_path
+    global emotion_model_path
+    global emotion_labels
+    global frame_window
+    global emotion_window
+
     def __init__(self):
         self.video = cv2.VideoCapture(0)
-        self.cascade = cv2.CascadeClassifier('C:/dev/finalProject/project/smile/detection_models/haarcascade_frontalface_default.xml')
+        self.cascade = cv2.CascadeClassifier(detection_model_path)
 
         #추가코드
+        self.emotion_classifier = load_model(emotion_model_path, compile=False)
+        # getting input model shapes for inference
+        self.emotion_target_size = self.emotion_classifier.input_shape[1:3]  # emotion_target_size = (48,48)
+        '''
+        self.image_size = ''
+        self.embeddings = []
+        self.emotion_count = 0
+        self.emotion_data = {}
+        '''
+
+
+
+
+
 
 
 
@@ -30,10 +61,53 @@ class VideoCamera:
     def get_frame(self):
         success, frame = self.video.read()
         self.gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        self.rgb = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)        #추가코드0924
+
+        #추카코드0924
         self.faces = self.cascade.detectMultiScale(self.gray,scaleFactor=1.1, minNeighbors=5)
+        #face = sorted(faces, reverse=True, key=lambda x:(x[2] - x[0]) * (x[3] - x[1]))[0]
+        for (fX, fY, fW, fH) in self.faces:
+
+            gray_face = self.gray[fY:fY + fH, fX:fX + fW]
+            #color_face = self.rgb[fY:fY + fH, fX:fX + fW]
+            gray_face = cv2.resize(gray_face,(48,48))
+            gray_face = gray_face.astype("float") / 255.0
+            gray_face = img_to_array(gray_face)
+            gray_face = np.expand_dims(gray_face, axis=0)      #(48,48,1)
+
+            emotion_prediction = self.emotion_classifier.predict(gray_face)[0]
+            emotion_probability = np.max(emotion_prediction)
+            emotion_label = np.argmax(emotion_prediction)
+            emotion_text = emotion_labels[emotion_label]    #happy, sad, surprise
+            emotion_window.append(emotion_text)
+
+            if len(emotion_window) > frame_window:
+                emotion_window.pop(0)
+
+            #emotion_mode = mode(emotion_window)
+            if emotion_text == 'happy':
+                #color = emotion_probability * np.asarray((255,0,0))
+                image = cv2.rectangle(frame, (fX, fY), (fX + fW, fY + fH), (255, 0, 0), 4)
+                cv2.putText(image, emotion_text, (fX, fY), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 0, 0), 4)
+            else:
+                #color = emotion_probability * np.asarray((0,255,0))
+
+                #image = cv2.rectangle(frame, (fX, fY), (fX + fW, fY + fH), (255,255,255), 2)
+                cv2.putText(frame, "", (fX, fY), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2)
+
+
+            #color = color.astype(int)
+            '''
+            image = cv2.rectangle(frame,(fX, fY),(fX + fW, fY + fH),(255,0,0),2)
+            cv2.putText(image,emotion_text,(fX, fY),cv2.FONT_HERSHEY_SIMPLEX,2,(11,52,243),2)
+            '''
+
+
+        '''
         for (x,y,w,h) in self.faces:
             image = cv2.rectangle(frame,(x, y), (x+w, y+h), (255, 0, 0), 2)
             cv2.putText(image, 'Face',(x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 2, (11, 52, 243), 2)
+        '''
 
         success, jpeg = cv2.imencode('.jpg',frame)
         return jpeg.tobytes()
