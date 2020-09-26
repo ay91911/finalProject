cb02da41de878b0365fcc5d8b020cbb8029f91a9
ay91911 <ay91911@gmail.com>
2020-09-26 12:36:18 +0900

    20200926-2

--- project/users/forms.py
+++ project/users/forms.py
@@ -1,11 +1,35 @@
 from django import forms
+from django.forms import ModelForm
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from smile.models import User
 
+'''
+GENDER=[
+    ('male', 'Male')
+    ('female', 'Female')
+]
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
+    gender = forms.CharField(label='Gender', widget=forms.Select(choices=GENDER))
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'email', 'password1', 'password2', 'gender']
+
+
+GENDER=[
+    ('male', 'Male')
+    ('female', 'Female')
+]
+'''
+
+class UserForm(ModelForm):
+    Password = forms.CharField(widget=forms.PasswordInput)
+    Gender = forms.CharField(widget=forms.Select)
+
+    class Meta:
+        model = User
+        fields = ['Email', 'Password', 'Username', 'Gender', 'Age']
+
