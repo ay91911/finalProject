f87804800570f4c26198c65017d10965f7b8b65e
roufspice <daeyoun.ha27@gmail.com>
2020-10-07 16:41:32 +0900

    20201007-16:40-dy

--- project/smile/urls.py
+++ project/smile/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
 
     path('',smile_views.index, name='index'),
     path('today_phrase_cam/', smile_views.video_today_phrase, name='today_phrase'),
+    path('today_phrase_cam_2/',smile_views.video, name='today_phrase_2'),
     path('neutral_cam/', smile_views.video_neutral, name='video_neutral'),
     path('level_1_cam/', smile_views.video_smile_level1, name='video_smile_level1'),
     path('level_2_cam/', smile_views.video_smile_level2, name='video_smile_level2'),
