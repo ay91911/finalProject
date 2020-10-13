1ad66d00e979748e8ead78adef49f97049cff0c9
roufspice <daeyoun.ha27@gmail.com>
2020-10-13 12:37:37 +0900

    20201013-12:37-dy

--- project/status/views.py
+++ project/status/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from smile.models import PHRASE, FACE, USER
 
 # Create your views here.
+
+def recent_smile(request):
+    user = USER.objects.get(pk=request.session["userEmail"])
+    context = {
+        # 'posts': FACE.filter(EMAIL=user).objects.all(),
+        'posts':FACE.filter(EMAIL=request.session["userEmail"]).objects.all(),
+    }
+
+    return render(request, 'status/compare.html',context)
